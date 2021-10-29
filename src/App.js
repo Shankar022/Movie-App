7c53482df575e36757aff010e9de1ff471175532
Shannkar022 <shankarweb22@gmail.com>
2021-10-29 11:12:40 +0530

    Sending get request using async await.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
-    const moviesHandler = () => {
-        fetch('https://swapi.dev/api/films/')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const transformedData = data.results.map((movieData) => {
-                    return {
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        releaseDate: movieData.release_date,
-                        openingText: movieData.opening_crawl
-                    };
-                });
-                setMovies(transformedData);
-            })
-            .catch((error)=>{
-                console.error('Error:', error);
-            })
+    // const moviesHandler = () => {
+    //     fetch('https://swapi.dev/api/films/')
+    //         .then((response) => {
+    //             return response.json();
+    //         })
+    //         .then((data) => {
+    //             const transformedData = data.results.map((movieData) => {
+    //                 return {
+    //                     id: movieData.episode_id,
+    //                     title: movieData.title,
+    //                     releaseDate: movieData.release_date,
+    //                     openingText: movieData.opening_crawl
+    //                 };
+    //             });
+    //             setMovies(transformedData);
+    //         })
+    //         .catch((error)=>{
+    //             console.error('Error:', error);
+    //         })
+    // }
+
+    const moviesHandler = async () => {
+        const response = await fetch('https://swapi.dev/api/films/');
+        const data = await response.json();
+        const transformedData = data.results.map((movieData) => {
+            return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                releaseDate: movieData.release_date,
+                openingText: movieData.opening_crawl
+            };
+        });
+        setMovies(transformedData);
     }
 
     return (
