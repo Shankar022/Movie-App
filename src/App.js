713056bfc6d6afb8ac6453de4a32f6ee0685a50d
Shannkar022 <shankarweb22@gmail.com>
2021-11-17 11:40:10 +0530

    POST data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState,useEffect,useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -32,20 +33,22 @@ function App() {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            const response = await fetch('https://react-https-2b6e7-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
             if (!response.ok) {
                 throw new Error('Something went wrong !');
             }
             const data = await response.json();
-            const transformedData = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    releaseDate: movieData.release_date,
-                    openingText: movieData.opening_crawl
-                };
-            });
-            setMovies(transformedData);
+            const loadedMovies = [];
+            for(const key in data){
+                loadedMovies.push({
+                    id:key,
+                    title:data[key].title,
+                    openingText:data[key].openingText,
+                    releaseDate:data[key].releaseDate
+                })
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -56,6 +59,17 @@ function App() {
         moviesHandler();
     },[moviesHandler])
 
+    const addMovieHandler= async (movie) => {
+        const response = await fetch('https://react-https-2b6e7-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',{
+            method:'POST',
+            body:JSON.stringify(movie),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
     let content = <p>No movies found.</p>;
     if (movies.length > 0) {
         content = <MoviesList movies={movies}/>;
@@ -69,6 +83,9 @@ function App() {
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
             <section>
                 <button onClick={moviesHandler}>Fetch Movies</button>
             </section>
